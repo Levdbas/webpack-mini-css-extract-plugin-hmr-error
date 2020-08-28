0f7576753028c9d274c72e497c2308fb619f6ebd
Erik van der Bas - PC <erik@basedonline.nl>
2020-08-28 14:47:42 +0200

    bump repo for new test

--- build/watch.js
+++ build/watch.js
@@ -5,10 +5,15 @@ global.watch = true;
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const compiler = webpack(webpackConfig);
+const { default: middleware } = require('webpack-dev-middleware');
 const express = require('express');
 const app = express();
 
-app.use(require("webpack-dev-middleware")(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath
-}));
-//app.use(require("webpack-hot-middleware")(compiler));
+app.use(
+  middleware(compiler, {
+      // webpack-dev-middleware options
+    })
+  );
+
+  app.listen(3000, () => console.log('Example app listening on port 3000!'));
+  //app.use(require("webpack-hot-middleware")(compiler));
